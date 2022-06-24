64af1e9dc7d127220c25e708fc96a8abe1a2c8ea
teekey1991 <matsunaetaiki@gmail.com>
2022-06-25 08:35:34 +0900

    B-5

--- B-5.py
+++ B-5.py
@@ -14,5 +14,36 @@
 最小値: 1
 平均値: 6
 """
+# 合計値
+def sum():
+    sum_number = 0
+    number = 0
+    for x in range(0, len(put)):
+        number = int(put[x])
+        sum_number = sum_number + number
+    return sum_number
 
-input("**")int(1 1 2 3 5 8 13 21)
+# 最大値
+def max():
+    check_number = 0
+    #max_number = 0
+    for x in range(0, len(put)):
+        number = int(put[x])
+        if number > check_number:
+            max_number = number
+                return max_number
+# 最小値
+#def min():
+
+
+# 平均値
+def ave():
+    ave_number = int(sum() / len(put))
+    return ave_number
+
+
+put = input("データを入力してください(スペース区切り) > ").split()
+print(f"合計値： {sum()}")
+print(f"最大値： {max()}")
+#print(f"最低値： {ave()}")
+print(f"平均値： {ave()}")
